79cdd44f2ca62308fd47dfaf8dda87d797a6ef03
Denis Davydov <davyov2002@list.ru>
2023-06-03 20:53:10 +0400

    Go-Trip
    Go-Trip

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// corousel
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center:true,
+    loop:true,
+    margin:20,
+    startPosition:0,
+    items:1,
+    responsive : {
+        640: {
+            items:3,
+            startPosition:1,
+        },
+        850: {
+            items:3,
+            startPosition:1,
+        },
+        1200 : {
+            items:3,
+            margin:30,
+        }
+    }
+});
+// Go to the previous item
+$('.slider__btn--prev').click(function() {
+    owl.trigger('prev.owl.carousel', [300]);
+})
+// Go to the next item
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+//nav-icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav= document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function(){
+    nav.classList.toggle('nav--mobile');
+    menuIcon.classList.toggle('menu-icon-active');
+}
+
